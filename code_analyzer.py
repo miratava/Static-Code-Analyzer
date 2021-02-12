1bfd6519e53d3cd9f41b1ab17041faede176c1a0
miratava <miratava@gmail.com>
2021-02-12 14:36:17 +0100

    third stage isn't tested

--- code_analyzer.py
+++ code_analyzer.py
@@ -1,6 +1,9 @@
 import enum
 from abc import ABC
 import re
+import sys
+import os
+import re
 
 
 class Status(enum.Enum):
@@ -22,12 +25,18 @@ class Input(ABC):
         pass
 
 
+class Arg(Input):
+    def get_input_path_to_file(self):
+        return sys.argv[0]
+
+
 class CLI(Input):
     def get_input_path_to_file(self):
         return input()
 
 
 class Analyzer:
+
     messages = {Message.too_long: "S001 Too long",
                 Message.not_four: "S002 Indentation is not a multiple of four",
                 Message.semicolon: "S003 Unnecessary semicolon",
@@ -39,27 +48,44 @@ class Analyzer:
 
     lines_with_message = {}
 
+    def __init__(self):
+        self.files = []
+
+    def get_file_path(self):
+        path = Arg().get_input_path_to_file()
+        if os.path.isfile(path):
+            self.files.append(os.path.basename(path))
+        elif os.path.isdir(path):
+            self.files = os.listdir(path)
+
+    def is_file_belong_python(self):
+        for file_name in self.files:
+            if not re.search("\.py$", file_name):
+                files.remove(file_name)
+            else:
+                files.replace(file_name, path + file_name)
+
     def validate_lines(self):
-        file_path = CLI().get_input_path_to_file()
-        line_number = 1
-        empty_lines = []
-        with open(file_path, "r") as f:
-            for line in f:
-                if self.is_line_empty_blanks(line):
-                    empty_lines.append(line_number)
-                self.validate_length(line, line_number)
-                self.validate_indentation(line, line_number)
-                self.validate_semicolon(line, line_number)
-                self.validate_two_space_before_comments(line, line_number)
-                self.validate_is_todo(line, line_number)
-                self.validate_for_more_than_two_blanks(empty_lines)
-                line_number += 1
+        for file_path in self.files:
+            line_number = 1
+            empty_lines = []
+            with open(file_path, "r") as f:
+                for line in f:
+                    if self.is_line_empty_blanks(line, file_path):
+                        empty_lines.append(line_number)
+                    self.validate_length(line, line_number, file_path)
+                    self.validate_indentation(line, line_number, file_path)
+                    self.validate_semicolon(line, line_number, file_path)
+                    self.validate_two_space_before_comments(line, line_number, file_path)
+                    self.validate_is_todo(line, line_number, file_path)
+                    self.validate_for_more_than_two_blanks(empty_lines, file_path)
+                    line_number += 1
         return Status.ok
 
-    def validate_length(self, line, number):
+    def validate_length(self, line, number, file_path):
         chars_amount = 79
         if len(line) - 1 > chars_amount:
-            self.add_new_error(Message.too_long, str(number))
+            self.add_new_error(file_path, Message.too_long, str(number))
 
     @staticmethod
     def find_last_space_index(line):
@@ -70,13 +96,13 @@ class Analyzer:
             else:
                 return line.index(char) - 1
 
-    def validate_indentation(self, line, number):
+    def validate_indentation(self, line, number, file_path):
         regex = "^\s+."
         if re.match(regex, line):
             if len(line[:self.find_last_space_index(line) + 1]) % 4 != 0:
-                self.add_new_error(Message.not_four, str(number))
+                self.add_new_error(file_path, Message.not_four, str(number))
 
-    def validate_semicolon(self, line, number):
+    def validate_semicolon(self, line, number, file_path):
         comment_symbol = "#"
         semicolon = ";"
         if comment_symbol in line:
@@ -85,9 +111,9 @@ class Analyzer:
             line_tmp = line
         if semicolon in line_tmp:
             if line_tmp.strip()[-1] == semicolon:
-                self.add_new_error(Message.semicolon, str(number))
+                self.add_new_error(file_path, Message.semicolon, str(number))
 
-    def validate_two_space_before_comments(self, line, number):
+    def validate_two_space_before_comments(self, line, number, file_path):
         comment_symbol = "#"
         two_space = "  "
         if comment_symbol in line:
@@ -95,15 +121,15 @@ class Analyzer:
                 return
             tmp_line = line[:line.index(comment_symbol)]
             if not tmp_line.endswith(two_space):
-                self.add_new_error(Message.two_space, str(number))
+                self.add_new_error(file_path, Message.two_space, str(number))
 
-    def validate_is_todo(self, line, number):
+    def validate_is_todo(self, line, number, file_path):
         comment_symbol = "#"
         todo = "todo"
         if comment_symbol in line:
             tmp_line = line[line.index(comment_symbol):]
             if todo in tmp_line.lower():
-                self.add_new_error(Message.todo, str(number))
+                self.add_new_error(file_path, Message.todo, str(number))
 
     @staticmethod
     def is_line_empty_blanks(line):
@@ -112,7 +138,7 @@ class Analyzer:
             return True
         return False
 
-    def validate_for_more_than_two_blanks(self, numbers: list):
+    def validate_for_more_than_two_blanks(self, numbers: list, file_path):
         if not numbers:
             return
         smaller_number = numbers[0]
@@ -121,31 +147,31 @@ class Analyzer:
             if number - smaller_number == 1:
                 count += 1
             if count > 2:
-                self.add_new_error(Message.two_blank, str(number + 1))
+                self.add_new_error(file_path, Message.two_blank, str(number + 1))
             smaller_number = number
 
-    def add_new_error(self, message, line_number: str):
-        if line_number not in self.lines_with_message:
-            self.lines_with_message[line_number] = []
-        if message not in self.lines_with_message.get(line_number):
-            self.lines_with_message[line_number].append(message)
+    def add_new_error(self, file_path, message, line_number: str):
+        if file_path not in self.lines_with_message:
+            self.lines_with_message[file_path] = []
+        if line_number not in self.lines_with_message.get(file_path):
+            self.lines_with_message[file_path][line_number] = []
+        if message not in self.lines_with_message.get(file_path).get(line_number):
+            self.lines_with_message[file_path][line_number].append(message)
 
     def get_error_messages(self):
-        numbers = [int(x) for x in self.lines_with_message.keys()]
-        numbers.sort()
-        [self.lines_with_message.get(str(number)).sort() for number in numbers]
+        for file, lines in self.lines_with_message.values():
+            numbers = [int(x) for x in lines.keys()].sort()
+            [lines.get(str(number)).sort() for number in numbers]
 
-        # for number in numbers:
-        #     self.lines_with_message.get(str(number)).sort()
-
-        return [f'Line {number}: {self.messages.get(message)}'
-                for number in numbers for message in self.lines_with_message.get(str(number))]
+            yield [f'{file}: Line {number}: {self.messages.get(message)}'
+                   for number in numbers for message in self.lines_with_message.get(str(number))]
 
 
 def main():
     analyzer = Analyzer()
     analyzer.validate_lines()
-    [print(x) for x in analyzer.get_error_messages()]
+    while True:
+        [print(x) for x in analyzer.get_error_messages()]
 
 
 main()
